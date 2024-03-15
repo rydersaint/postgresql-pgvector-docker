1b3f54a4eea25262e74c4bc55c6e438e95601d6a
Ryder Saint <rydersaint@gmail.com>
2024-03-15 12:11:14 +0900

    adding updates

--- setup_postgres.sh
+++ setup_postgres.sh
@@ -5,11 +5,17 @@
 # Wait for PostgreSQL to start
 sleep 10
 
+# Modify listen_addresses to listen on all interfaces
+sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgresql/16/main/postgresql.conf
+
 # Copy pg_hba.conf to the correct location
 cp /pg_hba.conf /etc/postgresql/16/main/pg_hba.conf
 
-# Create pgvector extension. Replace 'your_database' with your actual database name.
-# su - postgres -c "psql your_database -c 'CREATE EXTENSION pgvector;'"
+# Create the database if it doesn't exist (Replace 'vectortest' with your actual database name)
+su - postgres -c "psql -c \"SELECT 1 FROM pg_database WHERE datname = 'vectortest'\" | grep -q 1 || psql -c 'CREATE DATABASE vectortest;'"
+
+# Create pgvector extension
+su - postgres -c "psql vectortest -c 'CREATE EXTENSION vector;'"
 
 # Keep the container running
 tail -f /dev/null
